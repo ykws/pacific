32286d625b4e45354a62c73dcf9c12c657db089b
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2015-07-23 23:10:58 +0900

    プラグインを使い、モール開発実績にモール一覧を出力させる
    WordPress ダッシュボードでの作業
    * プラグイン Child Pages Shortcode を有効化
    * 対象の固定ページに以下コードを記述
    [child_pages size="pickup_thumbnail"]

--- functions.php
+++ functions.php
@@ -34,3 +34,10 @@ add_image_size('category_image', 658, 113, true);
 
 // モールイメージ用画像サイズ設定
 add_image_size('pickup_thumbnail', 302, 123, true);
+
+// Child Pages Shortcode の CSS の URL を変更します。
+function change_child_pages_shortcode_css() {
+    $url = get_template_directory_uri() . '/css/child-pages-shortcode/style.css';
+    return $url;
+}
+add_filter('child-pages-shortcode-stylesheet', 'change_child_pages_shortcode_css');
