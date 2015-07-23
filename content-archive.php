14f4ba23715f7d7dce49058dc94d83ab325db1bf
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2015-07-23 17:39:27 +0900

    アイキャッチ画像を利用し、記事ごとの画像を表示させる

--- content-archive.php
+++ content-archive.php
@@ -1,4 +1,7 @@
         <article <?php post_class(); ?>>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail('large_thumbnail', array('alt' => the_title_attribute('echo=0'), 'title' => the_title_attribute('echo=0'))); ?>
+            </a>
             <header class="entry-header">
                 <time pubdate="pubdate" datetime="<?php the_time('Y-m-d'); ?>" class="entry-date"><?php the_time(get_option('date_format')); ?></time>
                 <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
--- functions.php
+++ functions.php
@@ -16,3 +16,21 @@ register_nav_menus(
         'place_utility' => 'ユーティリティ'
     )
 );
+
+// アイキャッチ画像を利用できるようにします。
+add_theme_support('post-thumbnails');
+
+// アイキャッチ画像サイズ設定
+set_post_thumbnail_size(90, 90, true);
+
+// サイドバー用画像サイズ設定
+add_image_size('small_thumbnail', 61, 61, true);
+
+// アーカイブ用画像サイズ設定
+add_image_size('large_thumbnail', 120, 120, true);
+
+// サブページヘッダー用画像サイズ設定
+add_image_size('category_image', 658, 113, true);
+
+// モールイメージ用画像サイズ設定
+add_image_size('pickup_thumbnail', 302, 123, true);
