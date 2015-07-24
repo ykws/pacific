06b4dc48382a9497808f01f418215b5de1780533
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2015-07-24 16:09:34 +0900

    コラムやお知らせなど投稿記事に作成者名を表示させる
    投稿の各ページに作成者名と日付を表示させる対応はこれから。
    
    変更しているとツールバーが表示されなくなった。
    どの変更が悪さしているか？

--- archive.php
+++ archive.php
@@ -1,9 +1,17 @@
 <?php get_header(); ?>
-      <section id="contents">
-          <header class="page-header">
-              <h1 class="page-title"><?php single_cat_title(); ?></h1>
-          </header>
-          <div class="posts">
+     <section id="contents">
+         <header class="page-header">
+             <h1 class="page-title">
+<?php
+if (is_author()) :
+    echo esc_html(get_the_author_meta('display_name', get_query_var('author')));
+else :
+    single_cat_title();
+endif;
+?>
+            </h1>
+        </header>
+        <div class="posts">
 <?php
 if (have_posts()) :
     while (have_posts()) :
@@ -12,8 +20,8 @@ if (have_posts()) :
     endwhile;
 endif;
 ?>
-          </div>
+        </div>
 <?php get_template_part('back_to_top'); ?>
-      </section><!-- #contents end -->
+    </section><!-- #contents end -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- content-archive.php
+++ content-archive.php
@@ -4,6 +4,15 @@
             </a>
             <header class="entry-header">
                 <time pubdate="pubdate" datetime="<?php the_time('Y-m-d'); ?>" class="entry-date"><?php the_time(get_option('date_format')); ?></time>
+<?php
+if (!is_search()) :
+?>
+                <span class="author vcard">
+<?php the_author_posts_link(); ?>
+                </span>
+<?php
+endif;
+?>
                 <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
             </header>
             <section class="entry-content">
