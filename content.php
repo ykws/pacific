dac24cdd94a4e4a9cf307873aaba7bf1e448faf6
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2015-07-25 10:53:16 +0900

    投稿の各ページに作成者と日付を表示させる
    ツールバーが表示させるようになった。
    原因不明。

--- content.php
+++ content.php
@@ -5,4 +5,20 @@
             <section class="entry-content">
                 <?php the_content(); ?>
             </section>
+<?php
+if (is_single()) :
+?>
+            <div id="content_date_author">
+              <ul class="alignright">
+                <li>
+                  <time pubdate="pubdate" datetime="<?php the_time('Y-m-d'); ?>" class="entry-date"><?php the_time(get_option('date_format')); ?></time>
+                </li>
+                <li>
+                  <?php the_author_posts_link(); ?>
+                </li>
+              </ul>
+            </div>
+<?php
+endif;
+?>
         </article>
